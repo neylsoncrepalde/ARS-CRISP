6beee6153f3fe483513ca4b9e55e05957365adb5
neylsoncrepalde <neylsoncrepalde@gmail.com>
2017-04-20 09:58:37 -0300

    Major updates

--- 01_le_matrizes.R
+++ 01_le_matrizes.R
@@ -39,7 +39,7 @@ plot(g2); title(nomes_arquivos[2])
 
 g3 <- graph_from_adjacency_matrix(dados[[3]])
 plot(g3); title(nomes_arquivos[3])
-v
+
 g4 <- graph_from_adjacency_matrix(dados[[4]])
 plot(g4); title(nomes_arquivos[4])
 
@@ -49,3 +49,60 @@ plot(g2, edge.arrow.size=.2); title(nomes_arquivos[2])
 plot(g3, edge.arrow.size=.2); title(nomes_arquivos[3])
 plot(g4, edge.arrow.size=.2); title(nomes_arquivos[4])
 par(mfrow=c(1,1))
+
+g5 <- graph_from_adjacency_matrix(dados[[5]])
+g6 <- graph_from_adjacency_matrix(dados[[6]])
+g7 <- graph_from_adjacency_matrix(dados[[7]])
+g8 <- graph_from_adjacency_matrix(dados[[8]])
+g9 <- graph_from_adjacency_matrix(dados[[9]])
+g10 <- graph_from_adjacency_matrix(dados[[10]])
+g11 <- graph_from_adjacency_matrix(dados[[11]])
+g12 <- graph_from_adjacency_matrix(dados[[12]])
+g13 <- graph_from_adjacency_matrix(dados[[13]])
+g14 <- graph_from_adjacency_matrix(dados[[14]])
+g15 <- graph_from_adjacency_matrix(dados[[15]])
+g16 <- graph_from_adjacency_matrix(dados[[16]])
+g17 <- graph_from_adjacency_matrix(dados[[17]])
+g18 <- graph_from_adjacency_matrix(dados[[18]])
+g19 <- graph_from_adjacency_matrix(dados[[19]])
+g20 <- graph_from_adjacency_matrix(dados[[20]])
+g21 <- graph_from_adjacency_matrix(dados[[21]])
+g22 <- graph_from_adjacency_matrix(dados[[22]])
+g23 <- graph_from_adjacency_matrix(dados[[23]])
+g24 <- graph_from_adjacency_matrix(dados[[24]])
+g25 <- graph_from_adjacency_matrix(dados[[25]])
+g26 <- graph_from_adjacency_matrix(dados[[26]])
+g27 <- graph_from_adjacency_matrix(dados[[27]])
+g28 <- graph_from_adjacency_matrix(dados[[28]])
+g29 <- graph_from_adjacency_matrix(dados[[29]])
+g30 <- graph_from_adjacency_matrix(dados[[30]])
+g31 <- graph_from_adjacency_matrix(dados[[31]])
+g32 <- graph_from_adjacency_matrix(dados[[32]])
+g33 <- graph_from_adjacency_matrix(dados[[33]])
+g34 <- graph_from_adjacency_matrix(dados[[34]])
+
+grafos = list(g1,g2,g3,g4,g5,g6,g7,g8,g9,g10,
+              g11,g12,g13,g14,g15,g16,g17,g18,g19,g20,
+              g21,g22,g23,g24,g25,g26,g27,g28,g29,g30,
+              g31,g32,g33,g34)
+
+#####################################################
+### Montando uma tabela com as métricas de cada 
+
+# Vendo se há algum não conectado
+sapply(grafos, is.connected)
+
+densidades = sapply(grafos, edge_density)
+diametros = sapply(grafos, diameter)
+distancia_media = sapply(grafos, mean_distance)
+n = sapply(grafos, function(x) length(V(x)))
+transitividade = sapply(grafos, transitivity, type="global")
+
+banco <- data.frame(ego = nomes_arquivos,
+                    densidades = densidades,
+                    diametros = diametros,
+                    n = n,
+                    transitividade = transitividade,
+                    stringsAsFactors = F)
+View(banco)
+
